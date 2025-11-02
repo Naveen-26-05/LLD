fc25da3715e8bfe548292b68311bcc586811cda9
numasankar <naveenumasankar@gmail.com>
2025-11-02 22:48:43 +0530

    first commit

---
+++ CreationalPatter/Singleton/Main.java
@@ -0,0 +1,6 @@
+package Singleton;
+
+public class Main {
+    Singleton singleton1 = Singleton.GetInstance();
+    Singleton singleton2 = Singleton.GetInstance();
+}
---
+++ CreationalPatter/Singleton/Singleton.java
@@ -0,0 +1,15 @@
+package Singleton;
+
+public class Singleton {
+    
+    private static Singleton instance;
+    private Singleton(){
+
+    }
+    public static Singleton GetInstance(){
+       if(instance == null){
+        instance = new Singleton();
+       }
+       return instance;
+    }
+}
